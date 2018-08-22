32c7f902f944cb7805126f0e0bcf09f85dd17259
Danny Santos <dannyangelosanto@gmail.com>
2018-08-22 08:16:56 +0100

    Refactor

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -6,13 +6,13 @@ class CampaignsController < ApplicationController
   def show
     @campaign = Campaign.find(params[:id])
     @candidates = @campaign.candidates.uniq
-    @votes = {}
+    @candidates_and_scores = {}
     
     @candidates.each do |candidate|
-      @votes[candidate.name] = Vote.where(campaign_id: @campaign.id, candidate_id: candidate.id).during.count
+      @candidates_and_scores[candidate.name] = candidate.votes.where(campaign_id: @campaign.id, candidate_id: candidate.id).during.count
     end
     
-    @votes = Hash[@votes.sort_by{|k,v| v}.reverse]
+    @candidates_and_scores = Hash[@candidates_and_scores.sort_by{|k,v| v}.reverse]
     @uncounted_votes = @campaign.votes.pre + @campaign.votes.post
   end
 end
